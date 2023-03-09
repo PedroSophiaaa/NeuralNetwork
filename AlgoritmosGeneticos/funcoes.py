dc9bf1a0b9cad8ae126f5dc5e0e367e4f89db3fc
Pedro Henrique Sophia <pedro220045@ilum.cnpem.br>
2023-03-09 17:13:52 -0300

    A.01 and A.02 done

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,9 @@
- 
+def objetivo(ind):
+    """Utilizada para comparar individuos.
+    
+    Args:
+        ind: indivíduo
+        
+    Return:
+        A soma dos valores de cada caixa"""
+    return sum(ind)
